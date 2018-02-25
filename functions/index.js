f494eb1405fff22d9168122ce5775a1738812b7f
Charlie Jackson <charlie@charliejackson.com>
2018-02-25 08:40:15 +0000

    Get data from google apps scripts

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
-
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const fetch = require('node-fetch');
+require('dotenv').config();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+  const date = new Date();
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setDate(date.getDate() - 5);
+
+  fetch(
+    process.env.GOOGLE_SCRIPTS_API_URL,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        id: process.env.DAILY_METRICS_FORM_ID,
+        date: date.getTime(),
+      }),
+    }
+  )
+    .then(res => res.json())
+    .then(json => response.send(JSON.stringify(json)))
+    .catch(e => response.send(e.message));
 });
