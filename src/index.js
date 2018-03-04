efd05c5c5292e48848c6b1224e38aed4a12b9781
Charlie Jackson <charlie@charliejackson.com>
2018-03-04 14:36:29 +0000

    Added app component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 const root = document.getElementById("root");
 
 if (root === null) {
   throw new Error("#root does not exist on the page, could not load react");
 } else {
-  ReactDOM.render(<div>Hello</div>, root);
+  ReactDOM.render(<App />, root);
 }
